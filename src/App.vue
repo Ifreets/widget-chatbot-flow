b282e93fa9f9032ea4a4c587d3260c5e722fea7c
bbhtruonglm <truonglm.bbh@gmail.com>
2026-01-09 09:43:17 +0700

    update commit

--- src/App.vue
+++ src/App.vue
@@ -10,38 +10,37 @@
 </template>
 
 <script setup lang="ts">
-import { useCommonStore } from '@/stores'
-import WIDGET from 'bbh-chatbox-widget-js-sdk'
-import { onMounted } from 'vue'
+import { useCommonStore } from "@/stores";
+import WIDGET from "bbh-chatbox-widget-js-sdk";
+import { onMounted } from "vue";
 
-import AdBlocker from '@/components/AdBlocker.vue'
-import Loading from '@/components/Loading.vue'
-import Network from '@/components/Network.vue'
+import AdBlocker from "@/components/AdBlocker.vue";
+import Loading from "@/components/Loading.vue";
+import Network from "@/components/Network.vue";
 
+const commonStore = useCommonStore();
 
-const commonStore = useCommonStore()
-
-onMounted(() => initBbhWidget())
+onMounted(() => initBbhWidget());
 
 /**khởi động kết nối với bbh */
 function initBbhWidget() {
-  
-  getConversationInfo()
+  getConversationInfo();
 
   WIDGET.onEvent(() => {
     // đọc thông tin của khách hàng lần đầu load trang
-    getConversationInfo()
-  })
+    getConversationInfo();
+  });
 }
 /**đọc thông tin của khách hàng */
 async function getConversationInfo() {
-  try {    
+  try {
     // nếu có partner token thi lấy thống tin khách hàng theo kiểu mới
-    if(WIDGET.partner_token){
-      commonStore.conversation_info = await WIDGET.getClientInfo()
-    }else{
-      commonStore.conversation_info = await WIDGET.decodeClient()
-    } 
+    if (WIDGET.partner_token) {
+      commonStore.conversation_info = await WIDGET.getClientInfo();
+    } else {
+      // nếu không có partner token thì lấy thông tin khách hàng theo kiểu cũ
+      commonStore.conversation_info = await WIDGET.decodeClient();
+    }
   } catch (e) {
     console.log(e);
   }
